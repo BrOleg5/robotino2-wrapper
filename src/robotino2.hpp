e41c596321dee10b3e4a00eb24f0ffac28482852
BrOleg5 <oleg.brylyov@mail.ru>
2023-02-22 00:17:43 +0700

    Add Doxygen target in CMake to build documentation. Update minimal CMake version from 3.0 to 3.9 for to add Doxygen target. Fix CMake code style.

--- src/robotino2.hpp
+++ src/robotino2.hpp
@@ -1,5 +1,5 @@
 #ifndef ROBOTINO2_HPP
-#   define ROBOTINO2_HPP 
+#define ROBOTINO2_HPP 
 
 #include <iostream>
 #include <cmath>
@@ -22,56 +22,38 @@ using namespace rec::robotino::com;
 class Robotino2 : public Com
 {
 private:
-    /**
-     * Number of Robotino motors.
-     */
+    /// Number of Robotino motors.
     const unsigned char motor_num = 3;
 
-    /**
-     * Array of Robotino motors.
-     */
+    /// Array of Robotino motors.
     std::array<Motor, 3> motor;
 
-    /**
-     * Motors' actual positions in encoder ticks. 
-     */
+    /// Motors' actual positions in encoder ticks. 
     std::array<int, 3> actual_position;
     
-    /**
-     * Motors' actual velocities in rad/s. 
-     */
+    /// Motors' actual velocities in rad/s. 
     std::array<float, 3> actual_velocity;
 
-    /**
-     * Motors' actual currents in A. 
-     */
+    /// Motors' actual currents in A. 
     std::array<float, 3> actual_current;
 
-    /**
-     * Represents a Robotino motion drive.
-     */
+    /// Represents a Robotino motion drive.
     OmniDrive omniDrive;
 
-    /**
-     * Motor's velocity limit in rad/s.
-     */
+    /// Motor's velocity limit in rad/s.
     const float motor_vel_limit = 240;
 
-    /**
-     * Robot's linear speed limit in m/s.
-     */
+    /// Robot's linear speed limit in m/s.
     const float robot_lin_speed_limit = 0.8f;
 
-    /**
-     * Robot's angular velocity limit in rad/s.
-     */
+    /// Robot's angular velocity limit in rad/s.
     const float robot_vel_limit = static_cast<float>(PI);
 
 public:
     /**
      * Constructor. Start connection with Robotino4.
      * 
-     * @param ip_addr IP-addres of Robotino4.
+     * @param ip_addr IP-address of Robotino4.
      * @param sample_time sample time of motors' control and sensors' measurements in ms. Default 20 ms.
      */
     Robotino2(const std::string& ip_addr, unsigned int sample_time=20);
@@ -86,7 +68,6 @@ public:
      * 
      * @param error the error which caused this event.
      * @param errorString a human readable error description.
-     * @throws noting
      * @remark This function is called from outside the applications main thread.
      * This is extremely important particularly with regard to GUI applications.
      */
@@ -95,7 +76,6 @@ public:
     /**
      * This function is called if a connection to Robotino has been established.
      * 
-     * @throws nothing
      * @remark This function is called from outside the applications main thread. 
      * This is extremely important particularly with regard to GUI applications.
      */
@@ -104,7 +84,6 @@ public:
     /**
      * This function is called when a connection is closed.
      * 
-     * @throws nothing
      * @remark This function is called from outside the applications main thread.
      * This is extremely important particularly with regard to GUI applications.
      */
@@ -112,8 +91,6 @@ public:
 
     /**
      * This function is called whenever sensor readings have been updated.
-     * 
-     * @throws nothing
      */
     void updateEvent();
 
@@ -159,13 +136,15 @@ public:
      * 
      * @param num motor number.
      * @param speed setpoint speed in rad/s.
+     * @return True if motor set speed doesn't exceed the limit value and was set. Otherwise false.
      */
     bool set_motor_speed(unsigned char num, float speed);
 
     /**
      * Sets the setpoint speed of all motors.
      * 
-     * @param speed speed setpoints for all motors in rad/s.
+     * @param speeds speed setpoints for all motors in rad/s.
+     * @return True if motor set speeds don't exceed the limit value and were set. Otherwise false.
      */
     bool set_motor_speeds(const std::array<float, 3>& speeds);
 
@@ -215,6 +194,7 @@ public:
      * @param vy speed along y axis of robot's local coordinate system in m/s.
      * @param omega angular velocity of rotation in rad/s.
      * @remark This function is thread save. It should be called about every 100ms.
+     * @return True if robot set speed doesn't exceed the limit value and were set. Otherwise false.
      */
     bool set_robot_speed(float vx, float vy, float omega);
 
@@ -228,12 +208,24 @@ public:
     std::array<float, 3> robot_speed_to_motor_speeds(float vx, float vy, float omega);
 };
 
+/**
+ * Convert velocity from rpm to rad/s.
+ * 
+ * @param rpm velocity in rpm.
+ * @return Velocity in rad/s.
+*/
 inline float rpm2rads(float rpm) {
     return 2.f * PI * rpm / 60.f;
 }
 
+/**
+ * Convert velocity from rad/s to rpm.
+ * 
+ * @param rads velocity in rad/s.
+ * @return Velocity in rpm.
+*/
 inline float rads2rpm(float rads) {
     return 60.f * rads / (2.f * PI);
 }
 
-#endif
+#endif  // ROBOTINO2_HPP
